3714c7be30535215a3e1bab5fa087777e1bcae55
Law Zava <law@zava.dev>
2021-02-08 18:44:28 +0200

    :pencil: add additional comments

--- errors.go
+++ errors.go
@@ -3,6 +3,9 @@ package postal
 import "errors"
 
 var (
-	ErrInvalidCode   = errors.New("supplied code is invalid")
+	// ErrInvalidCode defines an error when the supplied code is not valid.
+	ErrInvalidCode = errors.New("supplied code is invalid")
+
+	// ErrStateNotFound defines an error when the supplied code did not match any of the US states.
 	ErrStateNotFound = errors.New("state not found by supplied code")
 )
